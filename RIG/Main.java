24c7972d30e6d3a555b0c8105187637416969f35
qsko <mi.zamelski@gmail.com>
2022-03-16 14:42:46 +0100

    refactor

--- RIG/Main.java
+++ RIG/Main.java
@@ -1,10 +1,22 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
 
 public class Main {
     public static void main(String[] args){
 
+
+
+        RandomInstationGenerator randomInstationGenerator = new RandomInstationGenerator();
+
+        Vector<Vector<Integer>> v = randomInstationGenerator.getEuclidean(3);
+        for(Vector<Integer> s :v){
+            System.out.println(s);
+        }
+
+
         Utils u  = new Utils();
+
 
         ArrayList<Integer> x = new ArrayList<>();
         x.add(10);
